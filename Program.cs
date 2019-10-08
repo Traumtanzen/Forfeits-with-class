8ea5ca5f19adaa1a70d4263c853bd9692479ab66
Traumtanzen <gvardser@gmail.com>
2019-10-08 19:51:47 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace TaskGame2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! Today we're playing forfeits.");
+            int PlrsNmbr = SetPlayers.PlayersNr();
+            List<string> PlrsNames = SetPlayers.PlayersNames(PlrsNmbr);
+            PlayRound(PlrsNmbr, PlrsNames);
+        }
+        static void PlayRound(int number, List<string> players)
+        {
+            string[] quests = File.ReadAllLines("ListOfForfeits.txt", Encoding.Default);
+            Console.WriteLine("Ok, all player's names are entered.");
+            Random rnd = new Random();
+
+            while (true)
+            {
+                Console.WriteLine("If you want to to see the names of all players, press 'p'; if you want to play, press any key; if you want to end the game, press 'q'");
+                string ans = Console.ReadLine();
+                if (ans == "q")
+                {
+                    Console.WriteLine("Bye!");
+                    break;
+                }
+                else if (ans == "p")
+                    for (int i = 0; i < number; i++)
+                    {
+                        Console.WriteLine(players[i]);
+                    }
+                else Console.WriteLine($"So, {players[rnd.Next(players.Count)]} should {quests[rnd.Next(quests.Length)]}!");
+            }
+        }
+    }
+}
---
+++ SetPlayers.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace TaskGame2
+{
+    public class SetPlayers
+    {
+        public static int PlayersNr()
+        {
+            GiveAnswer();
+            string inputNr = (Console.ReadLine());
+            int.TryParse(inputNr, out int actNumber);
+            while (true)
+            {
+                if (actNumber < 2 || actNumber > 10)
+                {
+                    Console.WriteLine("Sorry, there's too many or too little of you.");
+                    GiveAnswer();
+                }
+                else if (Enumerable.Range(2, 10).Contains(actNumber))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("You should enter a number of players.");
+                    GiveAnswer();
+                }
+            }
+            return actNumber;
+        }
+        public static void GiveAnswer()
+        {
+            Console.WriteLine("We need 2-10 players. How many of you are here now?");
+        }
+        public static List<string> PlayersNames(int playerCount)
+        {
+            List<string> player = new List<string>();
+            Console.WriteLine($"Ok! I see that there are {PlayersNr()} of you. Enter your names, please");
+            for (int counter = 0; counter < playerCount; counter++)
+            {
+                player.Add(Console.ReadLine());
+            }
+            return (player);
+        }
+    }
+}
